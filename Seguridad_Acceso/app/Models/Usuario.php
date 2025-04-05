f02d602c603de8fc349fad59fc3a80b8cad307e1
Jose Angel Grande <victor_espinoza@hotmail.com>
2025-04-04 19:18:53 -0700

    Modelo del dominio Usuario

--- Seguridad_Acceso/app/Models/Usuario.php
+++ Seguridad_Acceso/app/Models/Usuario.php
@@ -4,7 +4,6 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Authenticatable
 {
